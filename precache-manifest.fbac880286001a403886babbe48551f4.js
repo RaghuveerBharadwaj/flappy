e1a3b77e9bbeef7ed2089c071dcfcb3e3a620ef2
Raghuveer Bharadwaj <51020296+RaghuveerBharadwaj@users.noreply.github.com>
2024-02-09 22:51:39 +0530

    Updates

---
+++ precache-manifest.fbac880286001a403886babbe48551f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866f938682c6d24ad728227c658bbf2c",
+    "url": "/flappy/index.html"
+  },
+  {
+    "revision": "643635317eea7f501371",
+    "url": "/flappy/static/css/main.1915f0fa.chunk.css"
+  },
+  {
+    "revision": "4f109c9f68a87fbf409c",
+    "url": "/flappy/static/js/2.a83e661b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/flappy/static/js/2.a83e661b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "643635317eea7f501371",
+    "url": "/flappy/static/js/main.5f870c8a.chunk.js"
+  },
+  {
+    "revision": "0b0a478d321901844cea",
+    "url": "/flappy/static/js/runtime-main.186f8dac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flappy/precache-manifest.e5010cdccb1f150875254d679686d11e.js"
+  "/flappy/precache-manifest.fbac880286001a403886babbe48551f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flappy/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
